450aeeb23b199b7dffe826fbf2b19c89890e6ae8
Gabriela Goulart <101572408+gabrielagoulart@users.noreply.github.com>
2023-01-09 18:49:33 -0300

    introdução a require/exports/process

---
+++ require.js
@@ -0,0 +1,15 @@
+
+//Módulos nativos
+
+//const path = require('path')
+//console.log(path.basename(__filename))
+
+
+
+
+
+
+//Meus módulos
+
+const myModule = require('./exports')
+console.log(myModule)
